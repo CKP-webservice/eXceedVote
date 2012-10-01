8271d2e1c664ca7af99773be6d7f0f08207bf6a5
Kanin <arm.se.th@gmail.com>
2012-10-01 19:20:30 +0700

    Add Project model

---
+++ src/eXceedVote/src/Model/Project.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class Project {
+	private String projectName;
+	private String projectDetail;
+	private String imgURL;
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getProjectDetail() {
+		return projectDetail;
+	}
+	public void setProjectDetail(String projectDetail) {
+		this.projectDetail = projectDetail;
+	}
+	public String getImgURL() {
+		return imgURL;
+	}
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+}
