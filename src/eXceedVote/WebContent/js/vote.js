c203625ea1db58749a76363f3b84a886d0164d99
Pawis Chairoj <pawis.joe@gmail.com>
2012-10-02 14:22:20 +0700

    delete login temp

--- src/eXceedVote/WebContent/js/vote.js
+++ src/eXceedVote/WebContent/js/vote.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('select').change(function() {
       var optionValue = $('select').val();
-    $('#project1, #project2', '#project3').hide(350);
+    $('#project1, #project2').hide(350);
     switch(optionValue)
     {
     case "Project#1":
@@ -10,8 +10,6 @@ $(document).ready(function() {
     case "Project#2":
       $("#project2").show(350);
       break;
-    case "Project#3"
-      $("#project3").show(350);
     }
   });
   $('#vote').click(function() {
