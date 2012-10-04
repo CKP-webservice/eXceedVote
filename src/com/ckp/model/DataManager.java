e6277c6d7bc18724ebba1ef64ed666ac9a321820
Kanin <arm.se.th@gmail.com>
2012-10-04 08:02:07 +0700

    Delete load Account

--- src/com/ckp/model/DataManager.java
+++ src/com/ckp/model/DataManager.java
@@ -2,11 +2,6 @@ package com.ckp.model;
 
 public class DataManager {
 	
-	public static Account loadAccount(String username, String password) {
-		//TODO if can load from database, return object Account. else return null
-		return null;
-	}
-	
 	public static QuestionSet loadQuestionSet() {
 		
 		return null;
