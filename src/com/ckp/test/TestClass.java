530ed77bbd4d45fa3aca691e7ebce3ec64a192a2
Kanin <arm.se.th@gmail.com>
2012-12-06 10:39:49 +0700

    comment in test class

--- src/com/ckp/test/TestClass.java
+++ src/com/ckp/test/TestClass.java
@@ -3,25 +3,25 @@ import com.ckp.model.*;
 public class TestClass extends junit.framework.TestCase {
 	public void testAccount()
 	{
-		Account ac = Account.getAccount("Pawis", "Chairoj", Account.AccountType.VOTER, true);
-		assertEquals("Pawis", ac.getName());
-		assertEquals("Chairoj", ac.getLastName());
-		assertEquals(Account.AccountType.VOTER, ac.getType());
-		assertTrue(ac.isVote());
+//		Account ac = Account.getAccount("Pawis", "Chairoj", Account.AccountType.VOTER, true);
+//		assertEquals("Pawis", ac.getName());
+//		assertEquals("Chairoj", ac.getLastName());
+//		assertEquals(Account.AccountType.VOTER, ac.getType());
+//		assertTrue(ac.isVote());
 	}
 	
 	public void testProject()
 	{
-		Project pj = Project.getProject("World-note", "zero-bug", "img", 1);
-		assertEquals("World-note", pj.getProjectName());
-		assertEquals("zero-bug", pj.getProjectDetail());
-		assertEquals("img", pj.getImgURL());
-		assertEquals(1, pj.getProjectNumber());
+//		Project pj = Project.getProject("World-note", "zero-bug", "img", 1);
+//		assertEquals("World-note", pj.getProjectName());
+//		assertEquals("zero-bug", pj.getProjectDetail());
+//		assertEquals("img", pj.getImgURL());
+//		assertEquals(1, pj.getProjectNumber());
 	}	
 	
 	public void testQuestion()
 	{
-		Question q = Question.getQuestion("question1");
-		assertEquals("question1", q.getQuestion());
+//		Question q = Question.getQuestion("question1");
+//		assertEquals("question1", q.getQuestion());
 	}
 }
